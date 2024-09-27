236f6756cb7764213e79c0ddedc59d43ea4f83fb
Aji Nuansa Sengarie <ansengarie@gmail.com>
2024-09-27 21:02:08 +0700

    feat: create database schema and migration, mass assignment, ORM relationship & install filament

---
+++ app/Models/OfficeSpace.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class OfficeSpace extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'address',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'about',
+        'city_id',
+        'slug',
+    ];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(OfficeSpacePhoto::class);
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(OfficeSpaceBenefit::class);
+    }
+}
